c892c2f1754a5ee81fb1d18a57b9ca5234afc3bf
金龙熙 <308561217@qq.com>
2016-11-24 17:23:00 +0800

    联系人数据提交

--- www/js/services.js
+++ www/js/services.js
@@ -300,14 +300,22 @@ angular.module('starter.services', [])
 })
 .factory('Contact',function () {
     var personmainLists = [
-        {id:'1',img:'img/team/img6-md.jpg',name:'张总',company:'德阳工厂',address:'海宁，浙江，中国',phone:'15072200010',sex:'F',email:'12341@qq.com',labelId:1},
-        {id:'2',img:'img/team/img7-md.jpg',name:'小刘',company:'德阳工厂',address:'海宁，浙江，中国',phone:'15072200011',sex:'M',email:'12342@qq.com',labelId:2},
-        {id:'3',img:'img/team/img8-md.jpg',name:'Mike',company:'skytrading',address:'Paloalto,CA,USA',phone:'15072200012',sex:'F',email:'12343@qq.com',labelId:3},
-        {id:'4',img:'img/team/img9-md.jpg',name:'李四',company:'蓝天公司',address:'上海,中国',phone:'15072200013',sex:'M',email:'12344@qq.com',labelId:4},
-        {id:'5',img:'img/team/img10-md.jpg',name:'王总',company:'大海皮料',address:'苏州，江苏，中国',phone:'15072200014',sex:'F',email:'12345@qq.com',labelId:3},
-        {id:'6',img:'img/team/img11-md.jpg',name:'张三',company:'苏州希尔顿',address:'苏州 ，江苏，中国',phone:'15072200015',sex:'M',email:'12346@qq.com',labelId:2},
-        {id:'7',img:'img/team/img13-md.jpg',name:'王球童',company:'九桥高尔夫',address:'杭州，浙江，中国',phone:'15072200016',sex:'F',email:'12347@qq.com',labelId:1},
-        {id:'8',img:'img/team/img15-md.jpg',name:'FabioGrosso',company:'',address:'Turin，Italy',phone:'15072200017',sex:'M',email:'12348@qq.com'}
+        {id:'PERS_10001',img:'img/team/fenghao.png',name:'冯浩',company:'上海班富电子商务',address:'中国，浙江，杭州',
+          phone:'13801887706',sex:'F',email:'hao.feng@banff-tech.com',labelId:1},
+        {id:'PERS_10002',img:'img/team/zhangwenwen.jpeg',name:'张文文',company:'上海班富电子商务',address:'中国，上海，松江',
+          phone:'13162707331',sex:'F',email:'wenwen.zhang@banff-tech.com',labelId:2},
+        {id:'PERS_10003',img:'img/team/shenyinling.png',name:'沈寅麟',company:'上海班富电子商务',address:'中国，上海',
+          phone:'15000035538',sex:'F',email:'yinlin.shen@banff-tech.com',labelId:3},
+        {id:'PERS_10004',img:'img/team/',name:'王坤',company:'上海班富电子商务',address:'中国,上海，长宁',
+          phone:'18772115070',sex:'F',email:'kun.wang@banff-tech.com',labelId:4},
+        {id:'PERS_10005',img:'img/team/',name:'李宁',company:'上海班富电子商务',address:'中国，上海，长宁',
+          phone:'18702104254',sex:'F',email:'ning.li@banff-tech.com',labelId:3},
+        {id:'PERS_10006',img:'img/team/',name:'苏本坤',company:'上海班富电子商务',address:'中国 ，浙江，杭州',
+          phone:'18614055178',sex:'F',email:'benkun.su@banff-tech.com',labelId:2},
+        {id:'PERS_10007',img:'img/team/',name:'陈宇',company:'上海班富电子商务',address:'中国 ，浙江，杭州',
+          phone:'15910989807',sex:'F',email:'yu.chen@banff-tech.com',labelId:1},
+        {id:'PERS_10008',img:'img/team/jinlongxi.png',name:'金龙熙',company:'上海班富电子商务',address:'中国，上海，嘉定',
+          phone:'15618323607',sex:'F',email:'longxi.mei@banff-tech.com',labelId:1}
     ];
     return {
         getAll:function () {
