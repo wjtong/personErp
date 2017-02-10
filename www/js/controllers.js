a840f9c1300455508604a987fdfea1294614221a
金龙熙 <308561217@qq.com>
2017-02-10 10:31:15 +0800

    应用图片

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -406,19 +406,6 @@ angular.module('starter.controllers', ['ngCordova', 'ionic-datepicker', 'ionic-t
   $scope.activityList = Activity.getActivityInfo(id);
   $scope.personList = Activity.getAllPerson();
   //百度地图
-  $scope.tirarFoto = function(){
-    /*alert("开始定位");*/
-    var map = new BMap.Map("allmap");
-    map.centerAndZoom(new BMap.Point(116.331398,39.897445),11);
-    map.enableScrollWheelZoom(true);
-    baidu_location.getCurrentPosition(function(data){
-      alert(data)
-      $scope.data=data;
-      $rootScope.team=data;
-    }, function(err){
-      alert("错误："+err)
-    });
-  };
 })
 .controller('AboutMe',function ($scope, $rootScope, PersonData) {
   PersonData.getPersonInfo($rootScope.partyId , function (data){
